75711ff314821662b590e5e2fa434899a7ec9789
Wes Morgan <cap10morgan@gmail.com>
2015-09-11 08:58:14 -0600

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/bifrost "0.1.1-SNAPSHOT"
+(defproject democracyworks/bifrost "0.1.1"
   :description "Library for writing HTTP API gateways with Pedestal & core.async"
   :url "https://github.com/democracyworks/bifrost"
   :license {:name "Mozilla Public License"
